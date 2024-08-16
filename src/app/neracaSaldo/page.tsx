cc8c7eb4534c0fc2388c5b7cf726f8fc2143c25e
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-08-16 08:40:10 +0700

    finish all neraca

--- src/app/neracaSaldo/page.tsx
+++ src/app/neracaSaldo/page.tsx
@@ -1,6 +1,7 @@
 'use client'
+import Card from '@/components/elements/card/Card'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
-import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react'
+import { DatePicker, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react'
 import React from 'react'
 
 type Props = {}
@@ -8,6 +9,11 @@ type Props = {}
 const NeracaSaldo = (props: Props) => {
     return (
         <DefaultLayout>
+            <Card className='mb-4' >
+                <h1>Neraca Saldo</h1>
+                <p className='text-small text-gray' >Untuk menghitung total perbulan nya</p>
+                <DatePicker size='sm' aria-label='datepicker' className="max-w-[284px] bg-bone border-2 my-2 border-primary rounded-lg" />
+            </Card>
             <Table aria-label="Example static collection table">
                 <TableHeader>
                     <TableColumn>AKUN</TableColumn>
@@ -41,6 +47,14 @@ const NeracaSaldo = (props: Props) => {
                         <TableCell>{''}</TableCell>
                         <TableCell>1.500.000.000</TableCell>
                         <TableCell className='font-bold' >1.500.000.000</TableCell>
+                    </TableRow>
+                    <TableRow key="4">
+                        <TableCell className='font-bold' >TOTAL KEWAJIBAN DAN MODAL</TableCell>
+                        <TableCell>{''}</TableCell>
+                        <TableCell>{''}</TableCell>
+                        <TableCell>{''}</TableCell>
+                        <TableCell>{''}</TableCell>
+                        <TableCell className='font-bold' >1.550.000.000</TableCell>
                     </TableRow>
                 </TableBody>
             </Table>
